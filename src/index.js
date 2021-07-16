983ca58471bf7681729a8cf07fc0a9705e62a9b1
Christerle_Nounoute <christerlesn@gmail.com>
2021-07-16 13:20:32 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  const taskList = document.getElementById('tasks')
+  const taskName = document.getElementById('new-task-description')
+
+  function buildListItem(){
+    let task = document.createElement("li")
+    task.textContent = taskName.value + ' '
+
+    let button = document.createElement("button")
+    button.textContent = 'x'
+    task.appendChild(button)
+
+    return task
+  }
+
+  function addToDo(){
+    event.preventDefault()
+
+    task = buildListItem() 
+    list.appendChild(task)
+
+    event.target.reset()
+  }
+
+  form.addEventListener("submit", addToDo)
 });
